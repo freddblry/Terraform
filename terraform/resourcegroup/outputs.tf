8148a337999f4fa4c69ed8f9a3329a84eda737c8
frederic LEROY <74693233+freddblry@users.noreply.github.com>
2025-11-14 15:00:38 +0100

    Delete terraform/resourcegroup directory

--- terraform/resourcegroup/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  description = "Nom du Resource Group créé"
-  value       = azurerm_resource_group.rg.name
-}
-
-output "resource_group_location" {
-  description = "Localisation du Resource Group"
-  value       = azurerm_resource_group.rg.location
-}
