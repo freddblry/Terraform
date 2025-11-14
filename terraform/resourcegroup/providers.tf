0f6a328e8ae902db9b0278fbbbe49a483644b308
frederic LEROY <74693233+freddblry@users.noreply.github.com>
2025-11-14 15:01:21 +0100

    Add providers.tf for resourcegroup

---
+++ terraform/resourcegroup/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+
+  backend "local" {}
+}
+
+provider "azurerm" {
+  features {}
+}
